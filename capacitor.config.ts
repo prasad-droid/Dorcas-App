b05deaa699696b83e53ca2e04cfc8902deec7ad1
prasad <prasadkalvikatti@gmail.com>
2026-06-05 20:18:31 +0530

    feat: update application namespace and ID to align with new branding

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.psmcodes.dorcasaid',
+  appId: 'com.enats.dorcas',
   appName: 'Dorcas',
   webDir: 'dist',
   server: {
